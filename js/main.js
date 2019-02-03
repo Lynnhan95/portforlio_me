6915fbbb2c330ba7add4a49768d29238c43d0455
你的英文名Lynnhan95 <han536@purdue.edu>
2019-02-03 02:22:54 -0500

    123

--- js/main.js
+++ js/main.js
@@ -8,12 +8,40 @@ window.onscroll = function(){
     }else{
         topNavBar.classList.remove('sticky')
     }
+
+    let specialTags = document.querySelectorAll('div[data-x]')
+    var minIndex = 0
+// console.log(specialTags)
+for(let i =0; i< specialTags.length;i++){
+    specialTags[i].classList.remove('active')
 }
+for(let i =0; i< specialTags.length;i++){
+    // specialTags[i].classList.add('active')
+    
+   if(Math.abs(specialTags[i].offsetTop-window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)){
+       minIndex = i;
+   }
+}
+    // console.log(minIndex)
+    // specialTags[minIndex].classList.add('active')
+    let id = specialTags[minIndex].id 
+    // console.log(id)
+    let targetA = document.querySelector('a[href="#' + id + '"]')
+    let li = targetA.parentNode 
+    // console.log(li)
+    let childrens = li.parentNode.children
+    for(var i =0; i<childrens.length;i++){
+        childrens[i].classList.remove('active')
+    }
+    li.classList.add('active')
+
+
+}
+
 let menuTrigger = document.getElementsByClassName('menuTrigger')
 let slideTrigger = document.getElementsByClassName('submenu')
 let projectTrigger = menuTrigger[0].children[1].children
 
-console.log(projectTrigger)
 
 menuTrigger[0].onmouseenter = function(x){
     submenu.classList.add('active');
@@ -32,6 +60,9 @@ menuTrigger[0].onmouseleave = function(){
    
 }
 let aTags = document.querySelectorAll('nav.menu > ul > li >a ')
+
+
+        
 for(let i=0; i< aTags.length; i++){
     aTags[i].onclick = function(x){
         x.preventDefault()
@@ -39,12 +70,41 @@ for(let i=0; i< aTags.length; i++){
         let curHref = cur.getAttribute('href')
         
         let element = document.querySelector(curHref)
+        // console.log(element)
         // let rect = element.getBoundingClientRect()
-        var posY = element.offsetTop;
-        window.scrollTo(0,posY- 90)
+        let posY = element.offsetTop;
+
+        let n = 25 //动几次
+        let duration = 500 / n  //每次动的时间
+        let currentTop = window.scrollY
+        let targetTop = posY- 90
+        let s = targetTop - currentTop
+        
+        // Setup the animation loop.
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
+var coords = { y: currentTop }; // Start at (0, 0)
+let t = Math.abs((s/100) * 300)
+if(t > 500){
+    t = 500
+}
+var tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
+        .to({ y: targetTop }, t) // Move to (300, 200) in 1 second.
+        .easing(TWEEN.Easing.Quadratic.In) // Use an easing function to make the animation smooth.
+        .onUpdate(function() { // Called after tween.js updates 'coords'.
+         window.scrollTo(0,coords.y)
+        })
+        .start(); // Start the tween immediately.
         
     }
 }
+
+
+
 portfolio1.onclick= function(){
   portfolioBar.className = 'bar state-1'
 }
